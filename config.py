36a9b650938e3bdbc93846657b86834ea09f1abe
Tunzx64 <defftunz@gmail.com>
2025-01-23 23:29:25 -0300

    MusicPlay

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     SECRET_KEY = 'minha_chave'
-    SQLALCHEMY_TRACK = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "instance", "banco.db")}'
